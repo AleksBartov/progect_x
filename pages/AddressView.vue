01fcd2117465a88bc73b30185b75b2314697ebc7
Aleksandr <aleksandrbartov@yandex.ru>
2018-06-29 01:33:39 +0400

    just some changes

--- pages/AddressView.vue
+++ pages/AddressView.vue
@@ -1,40 +1,55 @@
 <template>
-  <div id="video-container">
-    <video autoplay loop poster="http://media.w3.org/2010/05/sintel/poster.png" muted="muted">
-      <source id="mp4" src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4">
-      <source id="webm" src="http://media.w3.org/2010/05/sintel/trailer.webm" type="video/webm">
-      <source id="ogv" src="http://media.w3.org/2010/05/sintel/trailer.ogv" type="video/ogg">
-      Your browser does not support the video tag. I suggest you upgrade your browser.
-      
-    </video>
-  </div>
+  <v-container fluid grid-list-xl>
+    <v-layout row justify-space-between>
+      <v-flex xs2>
+        <v-card dark color="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="letters">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row justify-space-around>
+      <v-flex xs2>
+        <v-card dark color="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="letters">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row justify-center>
+      <v-flex xs2>
+        <v-card dark color="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="letters">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
-<script>
-
-  export default {
-    data () {
-      return {
-        items: [
-              
-        ]
-      }
-    }
-  }
-</script>
-
-<style lang="stylus">
-  #video-container {
-      top:0%;
-      left:0%;
-      height:100%;
-      width:100%;
-      overflow: hidden;
-          position: absolute;
-  }
-  video {
-      position:absolute;
-      z-index:0;
-          width: 100%;
-  }
-</style>
